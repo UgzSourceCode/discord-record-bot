0a6b121beb19036df49b59a76079affc4587da74
Piotr 'Ugz' Bielski <pb93infa@gmail.com>
2022-04-06 22:12:27 +0200

    move CreateCampaignResponse to dedicated file 'createCampaignResponse.ts'

---
+++ src/types/mailerLiteApi/createCampaignResponse.ts
@@ -0,0 +1,9 @@
+export interface CreateCampaignResponse {
+  campaignType?: string;
+  date?: string;
+  accountId?: number;
+  campaignName?: string;
+  id?: number;
+  mailId?: number;
+  options?: unknown;
+}
