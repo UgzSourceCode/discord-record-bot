51568242f2af70eea20548744217f2c7109fe203
Piotr 'Ugz' Bielski <pb93infa@gmail.com>
2022-04-06 22:07:07 +0200

    implement util method for prepare MailerLite instance

--- src/utils/prepareMailerLiteClient.ts
+++ src/utils/prepareMailerLiteClient.ts
@@ -1,6 +1,11 @@
 import MailerLite from "mailerlite-api-v2-node";
+import { MailerLiteClient } from "../types/mailerLiteApi/mailerLiteClient";
 
 // TODO: add returned type
-export const prepareMailerLiteClient = (mailerLiteApiKey: string) => {
+export const prepareMailerLiteClient = (mailerLiteApiKey?: string): MailerLiteClient => {
+  if (!mailerLiteApiKey) {
+    throw `Not found "MAILERLITE_API_KEY" in your enviroments.`;
+  }
+
   return MailerLite(mailerLiteApiKey);
 };
